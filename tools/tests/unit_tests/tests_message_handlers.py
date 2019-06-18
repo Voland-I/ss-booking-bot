1910d6cc52a83a403b0e8cf0a3f84b956b5de862
Volodymyr Yukhymchuk <voland.private@gmail.com>
2019-06-18 16:29:45 +0300

    rewrite tests for new time-managing system

--- tools/tests/unit_tests/tests_message_handlers.py
+++ tools/tests/unit_tests/tests_message_handlers.py
@@ -10,10 +10,10 @@ from unittest import mock
 
 sys.path.insert(0, getcwd())
 
-from tools.message_handlers import get_now_local_datetime, \
+from tools.message_handlers import get_local_now, \
                                    make_row_from_item, \
                                    make_response_message, \
-                                   parse_time_from_msg, \
+                                   parse_time_delta, \
                                    create_answer
 
 
@@ -28,13 +28,8 @@ class TestGetNowLocalDatetime(unittest.TestCase):
     def setUp(self):
         self.UTC = pytz.UTC
         self.local_tz = get_localzone()
-
-        self.utc_now = datetime.utcnow()
-        self.utc_now = self.utc_now.astimezone(self.UTC)
-
+        self.utc_now = datetime.utcnow().astimezone()
         self.local_now = self.utc_now.astimezone(self.local_tz)
-        self.local_now_tmsp = self.local_now.timestamp()
-        self.local_now_str = self.local_now.isoformat()
 
         self.DATA_STUB['entities'][0]['timezone'] = self.local_tz.zone
 
@@ -42,41 +37,25 @@ class TestGetNowLocalDatetime(unittest.TestCase):
     def test_correct_local_time(self, dt_mock):
         dt_mock.utcnow.return_value = self.utc_now
 
-        result = get_now_local_datetime(self.DATA_STUB)
+        result = get_local_now(self.DATA_STUB['entities'][0]['timezone'])
 
         self.assertEqual(result, self.local_now)
 
 
 class TestMakeRowFromItem(unittest.TestCase):
 
-    def setUp(self):
-        utcnow = datetime.utcnow()
-        delta = timedelta(hours=1)
-        self.UTC = pytz.UTC
-        self.local_tz = get_localzone()
-        self.utc_now = utcnow.astimezone(self.UTC)
-
-        self.start_time = self.utc_now.astimezone(self.local_tz)
-        self.end_time = (self.utc_now + delta).astimezone(self.local_tz)
-
-        self.start_time_tmsp = self.start_time.timestamp()
-        self.end_time_tmsp = self.end_time.timestamp()
-
-        self.item_stub = {
-            'start_time': self.start_time_tmsp,
-            'end_time': self.end_time_tmsp,
-            'user_name': 'test_user',
-        }
+    ITEM_STUB = {
+        'start_time_str': '11:20',
+        'end_time_str': '11:40',
+        'user_name': 'test_user'
+    }
 
     def test_make_row_from_item(self):
-        control_start_time_str = self.start_time.strftime('%H:%M')
-        control_end_time_str = self.end_time.strftime('%H:%M')
+        result = make_row_from_item(self.ITEM_STUB)
 
-        result_row = make_row_from_item(self.item_stub)
-
-        self.assertIn(control_start_time_str, result_row)
-        self.assertIn(control_end_time_str, result_row)
-        self.assertIn(self.item_stub['user_name'], result_row)
+        self.assertIn(self.ITEM_STUB['start_time_str'], result)
+        self.assertIn(self.ITEM_STUB['end_time_str'], result)
+        self.assertIn(self.ITEM_STUB['user_name'], result)
 
 
 class TestMakeResponseMessage(unittest.TestCase):
@@ -84,10 +63,9 @@ class TestMakeResponseMessage(unittest.TestCase):
     HEADER_TEST = 'Test Header!(cool)'
 
     def setUp(self):
-        utcnow = datetime.utcnow()
         self.UTC = pytz.UTC
         self.local_tz = get_localzone()
-        self.utc_now = utcnow.astimezone(self.UTC)
+        self.utc_now = datetime.utcnow().astimezone(self.UTC)
 
         self.items = []
 
@@ -100,12 +78,16 @@ class TestMakeResponseMessage(unittest.TestCase):
         for i in range(4):
             start_time_utc = self.utc_now + i*interval
             end_time_utc = self.utc_now + i*interval + game_time_delta
-            start_time = start_time_utc.astimezone(self.local_tz)
-            end_time = end_time_utc.astimezone(self.local_tz)
+            start_time = start_time_utc.astimezone(self.local_tz).time()
+            end_time = end_time_utc.astimezone(self.local_tz).time()
+            start_delta = 60*start_time.hour + start_time.minute
+            end_delta = 60*end_time.hour + end_time.minute
+            start_end_delta = end_delta - start_delta
             user_name = f'test_user{i}'
             item = {
-                'start_time': start_time.timestamp(),
-                'end_time': end_time.timestamp(),
+                'start_delta': start_delta,
+                'end_delta': end_delta,
+                'start_end_delta': start_end_delta,
                 'start_time_str': start_time.strftime('%H:%M'),
                 'end_time_str': end_time.strftime('%H:%M'),
                 'user_name': user_name
@@ -144,81 +126,82 @@ class TestMakeResponseMessage(unittest.TestCase):
             self.assertIn(item, result)
 
 
-class TestParseTimeFromMsg(unittest.TestCase):
+class TestParseTimeDelta(unittest.TestCase):
+
+    ITEMS_STUB = {}
 
     def setUp(self):
-        utcnow = datetime.utcnow()
         delta = timedelta(minutes=30)
         self.UTC = pytz.UTC
         self.local_tz = get_localzone()
-        self.utc_now = utcnow.astimezone(self.UTC)
+        self.utc_now = datetime.utcnow().astimezone(self.UTC)
         self.local_now = self.utc_now.astimezone(self.local_tz)
         self.end_time = (self.utc_now + delta).astimezone(self.local_tz)
-        self.time_items = {}
-        self.time_items['correct'] = {
-            'start_time': self.local_now.timestamp(),
-            'end_time': self.end_time.timestamp(),
+
+        start_delta = 60*self.local_now.hour + self.local_now.minute
+        end_delta = 60*self.end_time.hour + self.end_time.minute
+        self.ITEMS_STUB['correct'] = {
+            'start_delta': start_delta,
+            'end_delta': end_delta,
             'start_time_str': self.local_now.strftime('%H:%M'),
             'end_time_str': self.end_time.strftime('%H:%M')
 
         }
 
-        self.time_items['incorrect_format'] = {
-            'start_time': self.local_now.timestamp(),
-            'end_time': self.end_time.timestamp(),
+        self.ITEMS_STUB['incorrect_format'] = {
+            'start_delta': start_delta,
+            'end_delta': end_delta,
             'start_time_str': self.local_now.strftime('%H-%M'),
             'end_time_str': self.end_time.strftime('%H-%M')
         }
 
     @mock.patch('tools.message_handlers.datetime')
     def test_with_correct_time_input(self, dt_mock):
-        start_time_str = self.time_items['correct']['start_time_str']
-        start_time_check = datetime.strptime(start_time_str, '%H:%M').time()
-        start_tmsp_check = datetime.combine(self.local_now, start_time_check).timestamp()
+        test_message = (f'book: {self.ITEMS_STUB["correct"]["start_time_str"]}'
+                        f'-{self.ITEMS_STUB["correct"]["end_time_str"]}')
 
-        end_time_str = self.time_items['correct']['end_time_str']
-        end_time_check = datetime.strptime(end_time_str, '%H:%M').time()
-        end_tmsp_check = datetime.combine(self.local_now, end_time_check).timestamp()
-        test_msg = f'Please, book me time: {start_time_str}-{end_time_str}'
+        start_delta_stub = self.ITEMS_STUB['correct']['start_delta']
+        end_delta_stub = self.ITEMS_STUB['correct']['end_delta']
 
-        dt_mock.utcnow.return_value = self.utc_now
         dt_mock.strptime.side_effect = lambda dt, f: datetime.strptime(dt, f)
-        dt_mock.combine.side_effect = lambda lt, gt: datetime.combine(lt, gt)
 
-        result = parse_time_from_msg(test_msg, self.local_tz)
+        result = parse_time_delta(test_message)
+        result_deltas, result_str = result
 
-        self.assertTupleEqual(result, (start_tmsp_check, end_tmsp_check))
+        self.assertTupleEqual(result_deltas, (start_delta_stub, end_delta_stub))
 
     def test_start_time_grater_than_end_time(self):
-        start_time_str = self.time_items['correct']['end_time_str']
-        end_time_str = self.time_items['correct']['start_time_str']
-        test_msg = f'incorrect time interval {start_time_str}-{end_time_str}'
 
-        result = parse_time_from_msg(test_msg, self.local_tz)
+        start_time_str = self.ITEMS_STUB['correct']['start_time_str']
+        end_time_str = self.ITEMS_STUB['correct']['end_time_str']
 
-        self.assertEqual(result, (None, None))
+        test_msg = f'incorrect time interval {end_time_str}-{start_time_str}'
+
+        result = parse_time_delta(test_msg)
+
+        self.assertTupleEqual(result, (None, None))
 
     def test_incorrect_time_format(self):
-        start_time_str = self.time_items['incorrect_format']['start_time_str']
-        end_time_str = self.time_items['incorrect_format']['end_time_str']
-        test_msg = f'Incorrect time-format {start_time_str}-{end_time_str}'
+        start_time_str = self.ITEMS_STUB['incorrect_format']['start_time_str']
+        end_time_str = self.ITEMS_STUB['incorrect_format']['end_time_str']
+        test_message = f'Incorrect time-format {start_time_str}-{end_time_str}'
 
-        result = parse_time_from_msg(test_msg, self.local_tz)
+        result = parse_time_delta(test_message)
 
         self.assertTupleEqual(result, (None, None))
 
     def test_only_one_time_entered(self):
-        start_time_str = self.time_items['correct']['start_time_str']
-        test_msg = f'There only one time entered: {start_time_str}'
+        start_time_str = self.ITEMS_STUB['correct']['start_time_str']
+        test_message = f'There only one time entered: {start_time_str}'
 
-        result = parse_time_from_msg(test_msg, self.local_tz)
+        result = parse_time_delta(test_message)
 
         self.assertTupleEqual(result, (None, None))
 
     def test_no_time_in_message(self):
         test_message = 'There is no time in message'
 
-        result = parse_time_from_msg(test_message, self.local_tz)
+        result = parse_time_delta(test_message)
 
         self.assertTupleEqual(result, (None, None))
 
@@ -235,7 +218,6 @@ class TestCreateAnswer(unittest.TestCase):
             'id': 'MI-6',
             'name': 'Top secret'
         },
-        'localTimestamp': '',
         'text': '',
         'entities': [{
             'timezone': '',
@@ -243,30 +225,29 @@ class TestCreateAnswer(unittest.TestCase):
     }
 
     def setUp(self):
-        utcnow = datetime.utcnow()
         delta = timedelta(minutes=30)
         self.UTC = pytz.UTC
         self.local_tz = get_localzone()
-        self.utc_now = utcnow.astimezone(self.UTC)
+        self.utc_now = datetime.utcnow().astimezone(self.UTC)
         self.start_time = self.utc_now.astimezone(self.local_tz)
         self.end_time = (self.utc_now + delta).astimezone(self.local_tz)
-        self.start_time_tmsp = self.start_time.timestamp()
-        self.end_time_tmsp = self.end_time.timestamp()
+        self.start_delta = 60*self.start_time.hour + self.start_time.minute
+        self.end_delta = 60*self.end_time.hour + self.end_time.minute
         self.start_time_str = self.start_time.strftime('%H:%M')
         self.end_time_str = self.end_time.strftime('%H:%M')
 
         self.DATA_STUB['text'] = 'Hello, I am a test message'
-        self.DATA_STUB['localTimestamp'] = (self.start_time - delta).isoformat()
         self.DATA_STUB['entities'][0]['timezone'] = self.local_tz.zone
 
         self.db_instance_mock = mock.MagicMock()
         self.db_instance_mock.get_all_items.return_value = []
         self.db_instance_mock.save.return_value = True
 
-    @mock.patch('tools.message_handlers.parse_time_from_msg')
+    @mock.patch('tools.message_handlers.parse_time_delta')
     @mock.patch('tools.message_handlers.make_response_message')
     def test_time_not_booked_header_accepted(self, make_r_mock, parse_t_mock):
-        parse_t_mock.return_value = self.start_time_tmsp, self.end_time_tmsp
+        parse_t_mock.return_value = (self.start_delta, self.end_delta), \
+                                    (self.start_time_str, self.end_time_str)
 
         make_r_mock.side_effect = lambda h, all_items: h
 
@@ -274,19 +255,20 @@ class TestCreateAnswer(unittest.TestCase):
 
         self.assertEqual(result, 'Accepted!(cool)')
 
-    @mock.patch('tools.message_handlers.parse_time_from_msg')
+    @mock.patch('tools.message_handlers.parse_time_delta')
     @mock.patch('tools.message_handlers.make_response_message')
-    def test_time_already_booked_header_rejected(self, make_mock, parse_mock):
-        parse_mock.return_value = self.start_time_tmsp, self.end_time_tmsp
+    def test_time_already_booked_header_rejected(self, make_r_mock, parse_t_mock):
+        parse_t_mock.return_value = (self.start_delta, self.end_delta), \
+                                    (self.start_time_str, self.end_time_str)
 
-        make_mock.side_effect = lambda h, all_items: h
+        make_r_mock.side_effect = lambda h, all_items: h
         self.db_instance_mock.save.return_value = False
 
         result = create_answer(self.db_instance_mock, self.DATA_STUB)
 
         self.assertIn('Rejected!', result)
 
-    @mock.patch('tools.message_handlers.parse_time_from_msg')
+    @mock.patch('tools.message_handlers.parse_time_delta')
     def test_time_not_entered_header_is_rejected(self, parse_t_mock):
         parse_t_mock.return_value = (None, None)
 
